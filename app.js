8f9337b466a883c00e76a54ecd25bdb50556740c
Geetanjali Goyal <geetanjaligoyal94@gmail.com>
2019-03-24 20:52:59 +0530

    Connecting Routes

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ router.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
+router.get('/PlayerInfo',function(req,res){
+  res.sendFile(path.join(__dirname+'/PlayerInfo.html'));
+});
+
+router.get('/Game',function(req,res){
+  res.sendFile(path.join(__dirname+'/Game.html'));
+});
+
+router.get('/HighScore',function(req,res){
+  res.sendFile(path.join(__dirname+'/HighScore.html'));
+});
+
+
 //add the router
 app.use('/', router);
 app.listen(process.env.port || 3000);
